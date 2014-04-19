36c14e4bb5fdf9cb609e148897ec7a6ce785ba2b
ShirleyW <wangsy710@gmail.com>
2014-04-19 18:26:36 -0400

    Scalable-UI

--- src/CreateUI.java
+++ src/CreateUI.java
@@ -8,10 +8,10 @@ import java.util.Random;
 import javax.swing.JFrame;
 
 public class CreateUI extends JFrame{
-	static final int WIDTH=500;
-	static final int HEIGHT=500;
+	static final int nodesSize=6;//node is a square, its width=height=nodesSize
+	final int WIDTH=nodesSize*PlayGround.WIDTH;
+	final int HEIGHT=nodesSize*PlayGround.HEIGHT;
 	BufferedImage offersetImage= new BufferedImage(WIDTH, HEIGHT,BufferedImage.TYPE_3BYTE_BGR);
-	Rectangle rect = new Rectangle(20, 40, 5 * 50, 5 * 35);
 	Snake snake;
 	PlayGround playGround;
 	Color colors[];
@@ -31,7 +31,7 @@ public class CreateUI extends JFrame{
 		 
 //		JFrame gui= new JFrame();
 		this.setTitle("Crazy Snake");
-		this.setBounds(100, 100, WIDTH+15, HEIGHT+40);
+		this.setBounds(100, 30, WIDTH+15, HEIGHT+30);
 //		this.setSize(800,600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//repaint();
@@ -43,13 +43,13 @@ public class CreateUI extends JFrame{
 		g2d.setColor(Color.white);
 		g2d.fillRect(0, 0, WIDTH, HEIGHT);
 		g2d.setColor(Color.black);
-		g2d.drawRect(0,0,500,500);
+		g2d.drawRect(0,0,WIDTH,HEIGHT);
 	
 //		snake.draw(g2d);
 //		snake2.draw(g2d);
 //		node.draw(g2d);
 		drawGround(g2d,playGround);
-		g.drawImage(offersetImage, 10, 30, null);
+		g.drawImage(offersetImage, 7, 25, null);
 	    
 //		 g.setColor(Color.BLACK);
 //	    g.drawRect(0,0,120,120);
@@ -59,9 +59,9 @@ public class CreateUI extends JFrame{
 		public void drawGround(Graphics2D g2d, PlayGround playGround) {
 			for (int i = 0; i < playGround.occupiedList.size(); i++) {
 				Point p=playGround.occupiedList.get(i);
-				System.out.println("drawSnake: x:"+p.getX()*5+"y:"+p.getY()*5);
+//				System.out.println("drawSnake: x:"+p.getX()*5+"y:"+p.getY()*5);
 				g2d.setColor(colors[p.getId()%100]);
-				g2d.drawRect(p.getX()*5, p.getY()*5, 5, 5);
+				g2d.drawRect(p.getX()*nodesSize, p.getY()*nodesSize, nodesSize, nodesSize);
 			}
 			
 		}
